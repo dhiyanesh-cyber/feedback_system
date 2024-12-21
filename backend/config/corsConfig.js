ba598e601624238528d67713b6aa6539318dad85
Dhiyanesh <dhiyaneshsasi03@gmail.com>
2024-12-21 18:50:21 +0530

    CORS update

--- backend/config/corsConfig.js
+++ backend/config/corsConfig.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 export const corsOptions = {
-  origin: 'http://localhost:5173', // Use the base URL without the `/auth` path
+  origin: 'http://https://feedback-hosting.vercel.app', // Use the base URL without the `/auth` path
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
