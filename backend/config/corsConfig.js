0c00f2c1884bc66bde70bd30dc690716b7e54f06
Dhiyanesh <dhiyaneshsasi03@gmail.com>
2024-12-28 09:54:12 +0530

    Form deletion updated

--- backend/config/corsConfig.js
+++ backend/config/corsConfig.js
@@ -1,7 +1,8 @@
 import cors from 'cors';
 
 export const corsOptions = {
-  origin: 'https://feedback-hosting.vercel.app', // Use the base URL without the `/auth` path
+  // origin: 'https://feedback-hosting.vercel.app', // Use the base URL without the `/auth` path
+  origin: "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
